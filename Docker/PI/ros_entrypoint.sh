12d33292756a1718b28edf97be408428ca824696
subArjun <118311762+subArjun@users.noreply.github.com>
2024-02-18 00:56:31 -0500

    Update ros_entrypoint.sh

--- Docker/PI/ros_entrypoint.sh
+++ Docker/PI/ros_entrypoint.sh
@@ -5,6 +5,6 @@ source /opt/ros/humble/setup.bash
 source /opt/ros_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export ROS_DOMAIN_ID=4
-ros2 launch tele_mapping tele_mapping_key.launch.py
+ros2 launch slam_swarm key_slam.launch.py
 
 exec "$@"
